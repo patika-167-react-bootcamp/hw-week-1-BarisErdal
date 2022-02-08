8e417480aa6c6b79e127e765b3a5ddd3c5184776
Baris Erdal <bariserdal.bme@gmail.com>
2022-02-08 20:35:15 +0300

    every ile yeniden yapıldı

--- hw-2.js
+++ hw-2.js
@@ -3,23 +3,15 @@
 
 Array.prototype.includesCI= function(val) {
 
-    if (!isNaN(val)) {
-        return this.includes(val);
-    }
-    
-
-let sonuc = false;
-   this.forEach(kelime=>{
-        if (!isNaN(kelime)) { return this.includes(val);} //arraydeki numbers ve boolean için
-
-        if(kelime.toLocaleLowerCase() === val.toLocaleLowerCase()) //arraydeki stringler için
-        { sonuc = true; }; 
-    
+let sonuc;
+    sonuc = this.every(item=>{
+       if (item.toLocaleLowerCase() === val.toLocaleLowerCase()) {
+           return false;
+       } else {return true;}
    });
 
-   return sonuc;
-       
-    
+   return sonuc ? false : true;
+           
 }
 
-console.log(["elmA","aRmut","PoRTaKaL","uzum",2,5,6,true].includesCI("ELMA"));
+console.log(["elmA","aRmut","PoRTaKaL","uzum"].includesCI("elma"));
